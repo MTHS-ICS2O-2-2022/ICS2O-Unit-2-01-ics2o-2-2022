36cc74a960de6c383ee0340149863bfa62a96477
ics2o-2-2022 <124388837+ics2o-2-2022@users.noreply.github.com>
2023-03-03 18:52:37 +0000

    from here: https://www.shellcheck.net/wiki/SC1090

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -41,5 +41,5 @@ sudo pip3 install cpplint
 # binary will be $(go env GOPATH)/bin/golangci-lint
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)/bin"
 
-# shellcheck source=~/.bashrc
+# shellcheck source=/dev/null
 source ~/.bashrc
