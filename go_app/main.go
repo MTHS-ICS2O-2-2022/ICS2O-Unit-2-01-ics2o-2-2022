df1ab4002f3b35bf51c07158b7d308f2a78433fa
ics2o-2-2022 <124388837+ics2o-2-2022@users.noreply.github.com>
2023-03-06 18:04:46 +0000

    Go app

---
+++ go_app/main.go
@@ -0,0 +1,15 @@
+// Created by: Mr Coxall
+// Created on: Sep 2020
+//
+// This program displays, "Hello, World!"
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function displays, "Hello, World!"
+	fmt.Println("Hello, World!")
+
+	fmt.Println("\nDone.")
+}
